cdf0bd8961e1a728add6199fb273935eb516218e
umidjon97 <vebdeveloper571632@mail.ru>
2021-11-03 17:13:49 +0500

    two commit

---
+++ src/main/java/com/example/spring_boot_pdp/SpringBootPdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_pdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPdpApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring_boot_pdp/lesson1/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_pdp.lesson1.service;
+
+import com.example.spring_boot_pdp.lesson1.dto.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> list();
+
+    Student view(int id);
+
+    String add(Student student);
+}
